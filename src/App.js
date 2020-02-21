9e3f7edf7966d82e313c8f1fa73d951d235ad98a
K0S4 <szymonkowalski142@gmail.com>
2020-02-21 23:01:43 +0100

    added page layout

--- src/App.js
+++ src/App.js
@@ -3,23 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    function changeColor (){
+        const contentBox = document.getElementById('content');
+        contentBox.classList.toggle('content-alt-bg')
+    }
+    return (
+    <>
+    <header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#news">About</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Projekty</a>
+                <div class="dropdown-content">
+                    <a href="liczby.html">Kalkulator Dat</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Polecane</a>
+                <div class="dropdown-content">
+                    <a href="http://ktorajestgodzina.tk">Która jest godzina?</a>
+                    <a href="http://sebson.tk">Sebson</a>
+                    <a href="http://cezario.tk">Cezario</a>
+                    <a href="http://kamiltrojan.tk">Kamil Trojan</a>
+                    <a href="https://boyfromzaun.tk/">BoyFromZaun</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</header>
+<content>
+    <div id="content">
+        <p id="witaj">Witaj!</p>
+        <span id="work">Strona Nadal Powstaje</span>
+        <button onClick={changeColor}>Zmien kolor tła</button>
     </div>
+</content>
+</>
   );
 }
 
